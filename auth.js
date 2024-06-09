79cf87f8bedb78a4adb0fb099fdd21ddaa3e2449
CAESTO MARCO TAMPUBOLON <marcocaesto@gmail.com>
2024-06-10 00:56:19 +0700

    fix: added secret to next js production

--- auth.js
+++ auth.js
@@ -3,5 +3,5 @@ import Google from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google],
-    secret: "4c95d46117c217b2d419c4fb94103f57",
+    secret: process.env.SECRET,
 });
