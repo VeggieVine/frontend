7730f15e3f7b91fcb99399d1461ecd1798f69b29
maharanikirei <155170622+maharanikirei@users.noreply.github.com>
2024-06-10 15:15:21 +0700

    update added product

---
+++ src/app/product/productDetails.jsx
@@ -0,0 +1,176 @@
+const productDetails = {
+    // Buah
+    'Apel Fuji China': {
+      name: 'Apel Fuji China',
+      imageSrc: '/assets/image/apel.png',
+      price: '40.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Apel Premium Pohon': {
+      name: 'Apel Premium Pohon',
+      imageSrc: '/assets/image/apel pohon.jpg',
+      price: '62.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Blueberry China': {
+      name: 'Blueberry China',
+      imageSrc: '/assets/image/blueberry.jpg',
+      price: '53.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Jeruk sunkist Valencia': {
+      name: 'Jeruk sunkist Valencia',
+      imageSrc: '/assets/image/jeruk sunkist.jpg',
+      price: '32.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Jeruk Lemon': {
+      name: 'Jeruk Lemon',
+      imageSrc: '/assets/image/jeruk lemon.jpg',
+      price: '28.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Jeruk Malang': {
+      name: 'Jeruk Malang',
+      imageSrc: '/assets/image/jeruk malang.jpg',
+      price: '30.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Pear Century China': {
+      name: 'Pear Century China',
+      imageSrc: '/assets/image/pear.jpg',
+      price: '25.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Kelengkeng Thailand': {
+      name: 'Kelengkeng Thailand',
+      imageSrc: '/assets/image/kelengkeng.jpg',
+      price: '34.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Kiwi': {
+      name: 'Kiwi',
+      imageSrc: '/assets/image/kiwi.jpg',
+      price: '69.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Belimbing': {
+      name: 'Belimbing',
+      imageSrc: '/assets/image/belimbing.jpg',
+      price: '29.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Semangka': {
+      name: 'Semangka',
+      imageSrc: '/assets/image/semangka.jpg',
+      price: '90.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Pepaya': {
+      name: 'Pepaya',
+      imageSrc: '/assets/image/pepaya.jpg',
+      price: '30.000',
+      category: 'Buah',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+  
+    // Sayur
+    'Bawang Bombai': {
+      name: 'Bawang Bombai',
+      imageSrc: '/assets/image/bawang bombai.jpg',
+      price: '35.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Bawang Putih': {
+      name: 'Bawang Putih',
+      imageSrc: '/assets/image/bawang putih.jpg',
+      price: '37.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Bawang Merah': {
+      name: 'Bawang Merah',
+      imageSrc: '/assets/image/bawang merah.jpg',
+      price: '25.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Jamur': {
+      name: 'Jamur',
+      imageSrc: '/assets/image/jamur.jpg',
+      price: '42.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Brokoli': {
+      name: 'Brokoli',
+      imageSrc: '/assets/image/brokoli.jpg',
+      price: '39.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Wortel': {
+      name: 'Wortel',
+      imageSrc: '/assets/image/wortel.jpg',
+      price: '17.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Tomat': {
+      name: 'Tomat',
+      imageSrc: '/assets/image/tomat.jpg',
+      price: '25.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Mentimun': {
+      name: 'Mentimun',
+      imageSrc: '/assets/image/timun.png',
+      price: '15.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Kentang': {
+      name: 'Kentang',
+      imageSrc: '/assets/image/kentang.jpeg',
+      price: '20.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Oyong': {
+      name: 'Oyong',
+      imageSrc: '/assets/image/oyong.jpeg',
+      price: '21.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Kol': {
+      name: 'Kol',
+      imageSrc: '/assets/image/kol.jpeg',
+      price: '8.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+    'Kol Unggu': {
+      name: 'Kol Unggu',
+      imageSrc: '/assets/image/kol unggu.jpeg',
+      price: '49.000',
+      category: 'Sayur',
+      description: 'Harga yang tercantum adalah harga untuk pembelian 1 Pack, Happy Shopping!!!'
+    },
+  };
+  
+  export default productDetails;
+  
