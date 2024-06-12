4e10a6427a34a36686e80a0e2471209b53363c82
CAESTO MARCO TAMPUBOLON <marcocaesto@gmail.com>
2024-06-12 20:06:45 +0700

    fix: eslint errors

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -138,13 +138,13 @@ function ProductPage() {
                 renderButtons={() => (
                     <div className="flex justify-center mt-4 space-x-4">
                         <button
-                            onClick={() => handleDetailClick(product)}
+                            // onClick={() => handleDetailClick(product)}
                             className="px-4 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
                         >
                             Detail
                         </button>
                         <button
-                            onClick={() => handleAddToCart(product)}
+                            // onClick={() => handleAddToCart(product)}
                             className="px-3 py-1 bg-green-500 text-white rounded hover:bg-green-600"
                         >
                             Add Keranjang
--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -3,7 +3,7 @@ const cart = []
 const handleCart = (state = cart, action) => {
     const product = action.payload
     switch (action.type) {
-        case "ADDITEM":
+        case "ADDITEM": {
             // Check if product already in cart
             const exist = state.find((x) => x.id === product.id)
             if (exist) {
@@ -14,7 +14,8 @@ const handleCart = (state = cart, action) => {
             } else {
                 return [...state, { ...product, qty: 1 }]
             }
-        case "DELITEM":
+        }
+        case "DELITEM": {
             const exist2 = state.find((x) => x.id === product.id)
             if (exist2.qty === 1) {
                 return state.filter((x) => x.id !== exist2.id)
@@ -23,6 +24,7 @@ const handleCart = (state = cart, action) => {
                     x.id === product.id ? { ...x, qty: x.qty - 1 } : x,
                 )
             }
+        }
 
         default:
             return state
